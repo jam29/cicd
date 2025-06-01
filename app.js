e952788d46468d57e1f3f5ff47688ff62efdc4c2
Jam <jam29n@gmail.com>
2025-06-01 23:02:40 +0200

    username

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello CI/CD2!'));
+app.get('/', (req, res) => res.send('Hello CI/CD3!'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server on http://localhost:${port}`));
