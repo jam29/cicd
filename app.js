df91f5d9a99c8fd1b91d30b0964c9700bbd4ffdf
Jam <jam29n@gmail.com>
2025-06-01 19:35:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello CI/CD!'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
